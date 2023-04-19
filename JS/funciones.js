e73e98f1c7aff2fe53d456824f605d4ec4775158
Alexander <dagc4173@yahoo.com>
2023-04-19 16:13:53 -0500

    cambio de concepto

---
+++ JS/funciones.js
@@ -0,0 +1,32 @@
+$(document).ready(() => {
+    $("menu-principal").load("Components\menuPrincipal.html");
+    $("#btnCargar").click(ClickBtnCargar);
+    $("#enviarFrm").click((event) => {
+        event.preventDefault();
+        const txtNombre = $("#nombre");
+        const txtPwd = $("#txtPwd");
+        let p = {
+            Nombre: txtNombre.val(),
+            Password: txtPwd.val()
+        };
+        $.post("https://dummyjson.com/products/add", p,
+            (rs) => {
+                console.log(rs);
+             })
+    })
+});
+
+
+function ClickBtnCargar() {
+    $.get("https://dummyjson.com/products", (rs) => {
+        let tbl = $("#tblProductos");
+        console.log(rs)
+        rs.products.forEach(p => {
+            tbl.append(`<tr>
+            <td>${p.id}</td>
+            <td>${p.title}</td>
+            <td><img width="100" src="${p.images[0]}"></td>
+            </tr>`);
+        });
+    });
+}
