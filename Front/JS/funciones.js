2bbd2f25250810d61d3607ad7d714e6b28bffa3a
Alexander <dagc4173@yahoo.com>
2023-04-26 14:57:15 -0500

    complementos

--- Front/JS/funciones.js
+++ Front/JS/funciones.js
@@ -33,3 +33,16 @@ function ClickBtnCargar() {
         });
     });
 }
+
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).stop(true,true).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).stop(true,true).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+});
