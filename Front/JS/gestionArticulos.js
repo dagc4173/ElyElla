6a251ec00466875f906a75643409a69263942d9a
Alexander <dagc4173@yahoo.com>
2023-05-25 01:58:20 -0500

    ok

--- Front/JS/gestionArticulos.js
+++ Front/JS/gestionArticulos.js
@@ -6,16 +6,16 @@ $(document).ready(() => {
 // Muestra los articulos almacenados en la base de datos 
 function CargarArticulos() {
     $.get(UrlApi + "/api/articulo", (items) => {
-      let tbl = $("#tblArticulo");
-      console.log(items)
-      items.forEach(p => {
-        tbl.append(`<tr>
+        let tbl = $("#tblArticulo");
+        console.log(items)
+        items.forEach(p => {
+            tbl.append(`<tr>
           <td>${p.Codigo}</td>
           <td>${p.Nombre}</td>
           <td>${p.Descripcion}</td>
           <td>${p.PrecioVenta}</td>
           <td>${p.Stock}</td>
-          <td><img width="100" src="${p.Imagen[0]}"></td>
+          <td><img width="100" src="/Front/Imagenes/${p.Imagen}"></td>
           <td class="d-flex justify-content-between">
               <div class="container-fluid text-center">
                   <button type="button" onClick="Actualizar(${p.IdArticulo})" class="btn btn-success actualizar-btn">Actualizar</button>
@@ -25,20 +25,20 @@ function CargarArticulos() {
               </div>
           </td>
           </tr>`);
-      });
+        });
     });
-  }
-  
-  function guardarProducto() {
+}
+
+function guardarProducto() {
     // Obtener los valores de los campos del formulario
-    const codigo = document.getElementById("codigo").value;
-    const nombre = document.getElementById("nombre").value;
-    const precio = document.getElementById("precio").value;
-    const stock = document.getElementById("stock").value;
-    const categoria = document.getElementById("categoria").value;
-    const descripcion = document.getElementById("descripcion").value;
-    const imagen = document.getElementById("imagen").value;
-    const subcategoria = document.getElementById("nombre-destino").value;
+    const codigo = $("#sku").val();
+    const nombre = $("#nombre").val();
+    const precio = $("#precio").val();
+    const stock = $("#stock").val();
+    const categoria = $("#categoria").val();
+    const descripcion = $("descripcion").val();
+    const imagen = $("#imagen").val();
+    const subcategoria = $("#nombre-destino").val();
     // Crear el objeto con los datos del producto
     const producto = {
         codigo: codigo,
@@ -50,24 +50,11 @@ function CargarArticulos() {
         imagen: imagen,
         subcategoria: subcategoria
     };
-    // Realizar la petición POST a la API para almacenar el producto
-    fetch(apiUrl + "/api/articulo", {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json"
-        },
-        body: JSON.stringify(producto)
-    })
-    .then(response => {
-        if (response.ok) {
-            // La petición se realizó correctamente
-            // Aquí puedes realizar alguna acción adicional, como mostrar un mensaje de éxito o redireccionar a otra página
-        } else {
-            // Ocurrió un error en la petición
-            console.error("Error al almacenar el producto.");
-        }
-    })
-    .catch(error => {
-        console.error("Error al realizar la petición:", error);
-    });
+
+    console.log(producto);
+
+    $.post(apiUrl + "/api/articulo", producto,
+    (rs) => {
+        console.log(rs);
+     })   
 }
